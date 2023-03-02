d49a587a50855c930ff0f1e156ecfbffc950ac71
premnath <premnath@squashapps.com>
2023-03-02 22:32:26 +0530

    api changed to https

--- src/routes/apiRoutes.jsx
+++ src/routes/apiRoutes.jsx
@@ -3,9 +3,9 @@ import { fetchUrl } from "../utilities/config";
 export const signup = fetchUrl("auth/register");
 export const loginApi = fetchUrl("api/login");
 export const validateUserApi = fetchUrl("api/validateuser");
-export const signinApi = "http://3.132.159.105:3000/auth/register";
-export const authMeApi = "http://3.132.159.105:3000/auth/me";
-export const googleSignInApi = "http://3.132.159.105:3000/auth/googlelogin";
+export const signinApi = "https://api.testrunz.com/authapp/auth/register";
+export const authMeApi = "https://api.testrunz.com/authapp/auth/me";
+export const googleSignInApi = "https://api.testrunz.com/authapp/auth/googlelogin";
 
 export const experimentsApi = fetchUrl("experiments/mypage");
 export const experimentsRunzApi = (id) => {
